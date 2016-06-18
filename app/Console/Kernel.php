08143a22f9c4ddee95ae462fb2da2946a7c8117d
web-ted <tgoltsios@gmail.com>
2016-06-18 12:39:36 +0300

    Added two cli commands to kernel.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         // Commands\Inspire::class,
+        Commands\WorkerImportXls::class,
+        Commands\Test::class,
     ];
 
     /**
