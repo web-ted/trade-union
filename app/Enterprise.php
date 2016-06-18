45f1b677463c8cdf5b1b62da17865dab6f0022ed
web-ted <tgoltsios@gmail.com>
2016-06-18 12:37:07 +0300

    Made the enterprise model table fillable.

--- app/Enterprise.php
+++ app/Enterprise.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Enterprise extends Model
 {
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
-    //
+    protected $fillable = [ 'name' ];
 }
