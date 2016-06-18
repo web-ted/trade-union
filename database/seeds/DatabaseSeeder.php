d7c9bdc474fdaf4d70ec40c682607a36ae005980
web-ted <tgoltsios@gmail.com>
2016-06-18 12:38:13 +0300

    Added workers and enteprise table seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call(WorkersTableSeeder::class);
+        $this->call(EnterprisesTableSeeder::class);
     }
 }
