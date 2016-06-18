da3c5eec0ce4f7bbdffaf529abd1437638fecc37
web-ted <tgoltsios@gmail.com>
2016-06-18 12:40:48 +0300

    Added model factory for seeding workers

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,53 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Worker::class, function (Faker\Generator $faker) {
+    return [
+        'active' => true,
+        'registration_number' => $faker->numberBetween(1, 400),
+        'registered_at' => $faker->date(),
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'father_name' => $faker->firstName,
+        'birth_date' => $faker->date('Y-m-d'),
+        'id_card' => $faker->creditCardNumber,
+        'phone' => $faker->phoneNumber,
+        'mobile_phone' => $faker->phoneNumber,
+        'email' => $faker->email,
+        'address' => $faker->address,
+        'region' => $faker->city,
+        'city' => $faker->city,
+        'hire_date' => $faker->date('Y-m-d'),
+        'insurance_number' => $faker->creditCardNumber,
+        'comment' => $faker->text(20),
+        'enterprise_id' => $faker->numberBetween(1, 5),
+        'enterprise_id' => $faker->numberBetween(1, 5),
+        'specialty_id' => $faker->numberBetween(1, 5),
+//        'created_at' => date("Y-m-d H:i:s"),
+        'created_at' => time(),
+//        'updated_at' => date("Y-m-d H:i:s"),
+        'updated_at' => time()
+    ];
+});
+
+$factory->define(App\Enterprise::class, function(Faker\Generator $faker) {
+    return [
+        'name' => $faker->company,
+        'address' => $faker->streetAddress,
+        'region' => $faker->city,
+        'city' => $faker->city,
+        'phone' => $faker->phoneNumber,
+        'fax' => $faker->phoneNumber,
+        'email' => $faker->email,
+        'founded' => $faker->year,
+        'workers_number' => $faker->numberBetween(10, 250),
+        'business_activity' => $faker->companySuffix,
+    ];
+});
+
+//$factory->define(App\Specialty::class, function(Faker\Generator) {
+//    return [
+//
+//    ];
+//});
