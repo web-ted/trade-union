7d493adab049c2566ee553cb8747df796810b88d
web-ted <tgoltsios@gmail.com>
2016-06-18 12:38:56 +0300

    Added Enterprises seeding factory on seed run.

---
+++ database/seeds/EnterprisesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EnterprisesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // factory(App\Enterprise::class, 50)->create();
+     }
+}
