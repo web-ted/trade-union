e8467530b396ee9033293863ef30b24ae152407f
Thodoris Goltsios <0DrktHhXUpa72x6GJsuZ>
2017-05-22 13:58:21 +0300

    Implemented database seeding for workers.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,33 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'name'           => $faker->name,
+        'email'          => $faker->email,
+        'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Worker::class, function (Faker\Generator $faker) {
+    return [
+        'active'              => 1,
+        'registration_number' => $faker->unique()->randomNumber(3),
+        'registered_at'       => $faker->date('Y-m-d', "3 days ago"),
+        'first_name'          => $faker->firstName,
+        'last_name'           => $faker->lastName,
+        'father_name'         => $faker->firstName,
+        'birth_date'          => $faker->date('Y-m-d', "3 year ago"),
+        'id_card'             => $faker->creditCardNumber,
+        'phone'               => $faker->phoneNumber,
+        'mobile_phone'        => $faker->phoneNumber,
+        'email'               => $faker->email,
+        'address'             => $faker->streetAddress,
+        'region'              => $faker->citySuffix,
+        'city'                => $faker->city,
+        'hire_date'           => $faker->date('Y-m-d', "10 years ago"),
+        'insurance_number'    => $faker->uuid,
+        'comment'             => $faker->realText(20),
+        'enterprise_id'       => 1,
+        'specialty_id'        => 1,
     ];
 });
--- database/seeds/WorkersTableSeeder.php
+++ database/seeds/WorkersTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class WorkersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
-        $this->call(WorkersTableSeeder::class);
+        factory(App\Worker::class, 200)->create();
     }
 }
