a516b6b84f816f8f33a67b4ec51270605685ad1f
Skliris Kostas <test@gmail.com>
2017-05-22 20:13:50 +0300

    Added datepicker directive in angular module.

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,10 +1,14 @@
 /**
  * Created by sulimo on 19/1/2016.
  */
-//angular.module('TradeUnion')
-//    .directive('pagination', function() {
-//        return {
-//           restrict: 'E',
-//
-//        };
-//    });
+angular.module('TradeUnion')
+    .directive('datePicker', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.datepicker({
+                    dateFormat: 'yy-mm-dd'
+                });
+            }
+        };
+    });
