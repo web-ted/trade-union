23b04e65c4ca62fc84c0dc686d64df94b78f3668
Thodoris Goltsios <tgoltsios@gmail.com>
2017-07-24 10:17:59 +0300

    Added accessors/mutators for date properties in worker model.

--- app/Worker.php
+++ app/Worker.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,15 +10,6 @@ class Worker extends Model
 {
     use SoftDeletes;
 
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-    	'birth_date',
-	    'hire_date',
-    ];
 
 	/**
 	 * That attributes that can be filled
@@ -25,6 +17,7 @@ class Worker extends Model
 	 */
     protected $fillable = [
         'registration_number',
+	    'registered_at',
         'active',
         'first_name',
         'last_name',
@@ -44,6 +37,125 @@ class Worker extends Model
         'enterprise_id',
         'specialty_id',
     ];
+
+	/**
+	 * Accessor for registered_at attribute
+	 * @param $registered_at
+	 * @return mixed
+	 */
+    public function getRegisteredAtAttribute($registered_at)
+    {
+    	if(!is_null($registered_at)) {
+    	    return Carbon::createFromFormat('Y-m-d H:i:s', $registered_at)->format('c');
+	    }
+
+	    return null;
+    }
+
+
+	/**
+	 * Mutator for registered_at attribute
+	 * @param $registered_at
+	 * @return $this
+	 */
+	public function setRegisteredAtAttribute($registered_at)
+	{
+		$this->attributes['registered_at'] = Carbon::parse($registered_at)->format('Y-m-d H:i:s');
+	}
+
+	/**
+	 * Accessor for birth_date attribute
+	 * @param $birth_date
+	 * @return mixed
+	 */
+    public function getBirthDateAttribute($birth_date)
+    {
+    	if(!is_null($birth_date)) {
+    	    return Carbon::createFromFormat('Y-m-d H:i:s', $birth_date)->format('c');
+	    }
+
+	    return null;
+    }
+
+
+	/**
+	 * Mutator for birth_date attribute
+	 * @param $birth_date
+	 * @return $this
+	 */
+	public function setBirthDateAttribute($birth_date)
+	{
+		$this->attributes['birth_date'] = Carbon::parse($birth_date)->format('Y-m-d H:i:s');
+	}
+
+	/**
+	 * Accessor for hire_date attribute
+	 * @param $hire_date
+	 * @return mixed
+	 */
+    public function getHireDateAttribute($hire_date)
+    {
+    	if(!is_null($hire_date)) {
+    	    return Carbon::createFromFormat('Y-m-d H:i:s', $hire_date)->format('c');
+	    }
+
+	    return null;
+    }
+
+
+	/**
+	 * Mutator for hire_date attribute
+	 * @param $hire_date
+	 * @return $this
+	 */
+	public function setHireDateAttribute($hire_date)
+	{
+		$this->attributes['hire_date'] = Carbon::parse($hire_date)->format('Y-m-d H:i:s');
+	}
+
+
+	/**
+	 * Accessor for updated_at attribute
+	 * @param $updated_at
+	 * @return mixed
+	 */
+	public function getUpdatedAtAttribute($updated_at)
+	{
+		if(!is_null($updated_at)) {
+			return Carbon::createFromFormat('Y-m-d H:i:s', $updated_at)->format('c');
+		}
+
+		return null;
+	}
+
+
+	/**
+	 * Accessor for created_at attribute
+	 * @param $created_at
+	 * @return mixed
+	 */
+	public function getCreatedAtAttribute($created_at)
+	{
+		if(!is_null($created_at)) {
+			return Carbon::createFromFormat('Y-m-d H:i:s', $created_at)->format('c');
+		}
+
+		return null;
+	}
+
+	/**
+	 * Accessor for deleted_at attribute
+	 * @param $deleted_at
+	 * @return mixed
+	 */
+	public function getDeletedAtAttribute($deleted_at)
+	{
+		if(!is_null($deleted_at)) {
+			return Carbon::createFromFormat('Y-m-d H:i:s', $deleted_at)->format('c');
+		}
+
+		return null;
+	}
 
     /**
      * Get the specialty record associated with the worker.
