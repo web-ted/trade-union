a4ccc2608007a9971f60b549853d92a522e8cac9
Thodoris Goltsios <tgoltsios@gmail.com>
2017-08-01 16:43:23 +0300

    Reformatted code in report controller.

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,26 +8,26 @@ use App\Http\Requests;
 
 class ReportController extends Controller
 {
-    public function exportAll($active=true)
-    {
-	    Excel::create('Members List', function ($excel) {
-	    $excel->sheet('Workers', function ($sheet) {
-		    $sheet->setOrientation('landscape');
-		    $sheet->freezeFirstRow();
-		    $sheet->loadView('excel.workers', ['members' => Worker::all()]);
-	    });
+	public function exportAll($active = true)
+	{
+		Excel::create('Members List', function ($excel) {
+			$excel->sheet('Workers', function ($sheet) {
+				$sheet->setOrientation('landscape');
+				$sheet->freezeFirstRow();
+				$sheet->loadView('excel.workers', ['members' => Worker::all()]);
+			});
 
-	    $excel->sheet('Enterprises', function ($sheet) {
-		    $sheet->setOrientation('landscape');
-		    $sheet->loadView('excel.enterprises', ['members' => Enterprise::all()]);
-	    });
+			$excel->sheet('Enterprises', function ($sheet) {
+				$sheet->setOrientation('landscape');
+				$sheet->loadView('excel.enterprises', ['members' => Enterprise::all()]);
+			});
 
-	    $excel->sheet('Specialties', function ($sheet) {
-		    $sheet->setOrientation('landscape');
-		    $sheet->loadView('excel.specialties', ['members' => Specialty::all()]);
-	    });
-    })->download('xlsx');
-    }
+			$excel->sheet('Specialties', function ($sheet) {
+				$sheet->setOrientation('landscape');
+				$sheet->loadView('excel.specialties', ['members' => Specialty::all()]);
+			});
+		})->download('xlsx');
+	}
 
 
 }
